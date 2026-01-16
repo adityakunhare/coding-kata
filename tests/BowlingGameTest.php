06b9e6331bde5fff896f8b571833ee9d1db0ed28
Aditya Kunhare <honey.kunhare@gmail.com>
2026-01-17 00:45:03 +0530

    bowling game kata processing.

---
+++ tests/BowlingGameTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ClassName
+ */
+class BowlingGameTest extends TestCase 
+{
+	public function test_if_it_is_a_gutter_game()
+	{
+		$game = new Game();
+			
+	}	
+}
